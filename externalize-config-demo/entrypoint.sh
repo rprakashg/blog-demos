eae7944e119a11c5a5925d7383c84119b3544059
Ram Gopinathan <rprakashg@outlook.com>
2017-09-21 22:16:35 -0700

    cleanup

--- externalize-config-demo/entrypoint.sh
+++ externalize-config-demo/entrypoint.sh
@@ -1,36 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Downloading config.json from git repository"
+echo "Downloading configuration files from git repository"
+wget  $GIT_REPO/$LABEL/$PATH/$APP_NAME.json
+wget  $GIT_REPO/$LABEL/$PATH/$APP_NAME-$PROFILE.json
 
-wget -O externalizeconfigdemo.json $GIT_REPO/$LABEL/$PATH/$APP.json
-
-JSON=""
-
-case $PROFILE in
-    "production")
-    echo "Downloading production config"
-    wget -O externalizeconfigdemo.json $GIT_REPO/$LABEL/$PATH/$APP-production.json
-    echo "Merging configs"
-    ;;
-
-    "staging")
-    echo "Downloading staging config"
-    wget -O externalizeconfigdemo.json $GIT_REPO/$LABEL/$PATH/$APP-staging.json
-    echo "Merging configs"
-
-    ;;
-
-    "development")
-    wget -O externalizeconfigdemo.json $GIT_REPO/$LABEL/$PATH/$APP-development.json
-    echo "Merging configs"
-
-    ;;
-esac
+cp $APP_NAME.json ./configs/$APP_NAME.json
+cp $APP_NAME-$PROFILE.json ./configs/$APP_NAME-$PROFILE.json
 
 CMD=$1
 echo "Command :" $CMD
-
-PROPS=""
 
 JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses -Duser.timezone=America/Los_Angeles -Dhttps.protocols=TLSv1.1,TLSv1.2 -Djava.security.egd=file:/dev/./urandom"
 
@@ -39,7 +17,7 @@ echo $JAVA_OPTS
 case "$CMD" in
     "start")
         echo "Starting SpringBoot application"
-        exec java $JAVA_OPTS -jar /app.jar --spring.application.json=$JSON
+        exec java $JAVA_OPTS -jar /app.jar --spring.config.location="./configs/$APP_NAME.yml, ./configs/$APP_NAME-$PROFILE.yml"
     ;;
 
     * )
--- externalize-config-demo/run.sh
+++ externalize-config-demo/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -d -p 80:8080 -e PROFILE=production rprakashg/externalize-config-demo .
+docker run -d -p 80:8080 -e PROFILE=production -e GIT_REPO= -e LABEL=master -e PATH="" \
+    -e APP_NAME="application" rprakashg/externalize-config-demo .

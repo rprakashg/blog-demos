80cbb6153d2c38c215da29edbf7860bbc6f38eb0
Ram Gopinathan <rprakashg@outlook.com>
2017-09-22 09:52:17 -0700

    updated run

--- externalize-config-demo/run.sh
+++ externalize-config-demo/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d -p 80:8080 -e PROFILE=staging -e GIT_REPO="https://raw.githubusercontent.com/rprakashg/blog-demos" \
+docker run -d -p 80:8080 -e PROFILE=production -e GIT_REPO="https://raw.githubusercontent.com/rprakashg/blog-demos" \
     -e LABEL=master -e REL_PATH="externalize-config-demo/src/main/resources" \
     -e APP_NAME="application" rprakashg/externalize-config-demo
